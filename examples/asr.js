4876925dc3407b99272c9bfa0de0d22a1c88d469
Sergey Dmitriev <serge.dmitriev@gmail.com>
2019-08-29 10:24:04 +0700

    change request promise to node-fetch

--- examples/asr.js
+++ examples/asr.js
@@ -4,6 +4,9 @@ const ACCESS_TOKEN = '6SQV3DEGQWIXW3R2EDFUMPQCVGOEIBCR';
 
 const witai = new witaiSpeech.ASR({token: ACCESS_TOKEN});
 
-witai.recognize('./demo.wav').then((result) => {
-  console.log('res', result);
+witai.recognize('./demo.wav').then(res => {
+  console.log('res.status', res.status)
+  return res.json();
+}).then((result) => {
+  console.log('result json', result);
 });
